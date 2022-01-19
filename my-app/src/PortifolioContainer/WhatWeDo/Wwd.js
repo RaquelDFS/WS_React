8dc96493906f8f401bb52a427a3b8a93bdd2ad71
Raquel Fonseca <raquelfonseca.rf@gmail.com>
2022-01-19 17:33:16 -0300

    1/2 tables

--- my-app/src/PortifolioContainer/WhatWeDo/Wwd.js
+++ my-app/src/PortifolioContainer/WhatWeDo/Wwd.js
@@ -5,44 +5,52 @@ const Wwd = () => {
   return (
     <div className="wwd-container">
       <div className="wwd-principal">
-        <div className="colz middle-colz"/>
-        <h3 className='subtitle'>Lorem Ipsum</h3>
+        <div className="colz middle-colz" />
+        <h3 className="subtitle">Lorem Ipsum</h3>
         <h1>
-                {" "}
-                Lorem Ipsum is simply <em className="pink-Lorem">dummy text.</em>
-              </h1>
-              <span className="AboutUs-role-tagline">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. 
-              </span>
+          {" "}
+          Lorem Ipsum is simply <em className="pink-Lorem">dummy text.</em>
+        </h1>
+        <span className="AboutUs-role-tagline">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s.
+        </span>
       </div>
       <div className="wwd-blocks">
         <div className="blocks">
-          <h2 className="title-block">Lorem Impsum</h2>
-          <p className="paragraph-block">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy
-          </p>
-          <div className="blocks">
-            <h2 className="title-block">Lorem Impsum</h2>
-            <p className="paragraph-block">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy
-            </p>
-          </div>
-          <div className="blocks">
-            <h2 className="title-block">Lorem Impsum</h2>
-            <p className="paragraph-block">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy
-            </p>
-          </div>
-          <div className="blocks">
-            <h2 className="title-block">Lorem Impsum</h2>
-            <p className="paragraph-block">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy
-            </p>
-          </div>
+          <ul>
+            <li className="left-grid">
+              <h2 className="title-block">Lorem Impsum</h2>
+              <p className="paragraph-block">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+              </p>
+              <div className="blocks">
+                <h2 className="title-block">Lorem Impsum</h2>
+                <p className="paragraph-block">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy
+                </p>
+              </div>
+            </li>
+            <li className="left-grid">
+              <h2 className="title-block">Lorem Impsum</h2>
+              <p className="paragraph-block">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+              </p>
+              <div className="blocks">
+                <h2 className="title-block">Lorem Impsum</h2>
+                <p className="paragraph-block">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy
+                </p>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
