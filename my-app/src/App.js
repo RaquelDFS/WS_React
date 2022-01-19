d694c653e28edaf91cfd1544fc43e8b3e2e53120
Raquel Fonseca <raquelfonseca.rf@gmail.com>
2022-01-19 12:56:43 -0300

    start aboutUs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Profile from "./PortifolioContainer/Home/Profile";
 import NavBarSection from "./PortifolioContainer/Nav_bar";
+import AboutUs from "./PortifolioContainer/AboutUs/AboutUs";
 
 function App() {
   return (
     <div className="App">
       <NavBarSection/>
-      <Profile />
+      <Profile/>
+      <AboutUs/>
     </div>
   );
-}
+};
 
 export default App;
