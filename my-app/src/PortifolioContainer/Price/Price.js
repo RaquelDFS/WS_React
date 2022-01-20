67f72468b74eb47f76be22526acc0b46626dee7f
Raquel Fonseca <raquelfonseca.rf@gmail.com>
2022-01-19 21:21:09 -0300

    start price tag

--- my-app/src/PortifolioContainer/Price/Price.js
+++ my-app/src/PortifolioContainer/Price/Price.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Price.css'
 const Price = () => {
-  return <article className='conteiner-price'>
+  return <div className='conteiner-price'>
              <div className="wwd-principal">
         <div className="colz middle-colz" />
         <h3 className="subtitle" id="prices">Prices</h3>
@@ -26,8 +26,92 @@ const Price = () => {
     </fieldset>
   </div>
 </div>
-      
-  </article>;
+  <section>
+      <div className='sessao-price-tag'>
+        
+        <div className='price-component-first'>
+        <div className='price-tag'>
+          <h1>Lorem Ipsum</h1>
+          <p className='price-price'><em className="pink-Lorem">R$ 149<small>/month</small></em></p>
+    </div>
+          <ul>
+            <li className='item-price have'>
+              <p>Lorem ipsum isso tem</p>
+            </li>
+            <li className='item-price have'>
+              <p>Lorem ipsum isso tem</p>
+            </li>
+            <li className='item-price not-have'>
+              <p>Lorem ipsum isso n tem</p>
+            </li>
+            <li className='item-price not-have'>
+              <p>Lorem ipsum isso n tem</p>
+            </li>
+            <li className='item-price not-have'>
+              <p>Lorem ipsum isso n tem</p>
+            </li>
+            <li>
+              <button className='btn not'>Comprar</button>
+            </li>
+          </ul>
+        </div>
+        <div className='price-component-second'>
+        <div className='price-tag'>
+          <h1>Lorem Ipsum</h1>
+          <p className='price-price'><em className="pink-Lorem">R$ 149<small>/month</small></em></p>
+    </div>
+          <ul>
+            <li className='item-price have'>
+              <p>Lorem ipsum isso tem</p>
+            </li>
+            <li className='item-price have'>
+              <p>Lorem ipsum isso tem</p>
+            </li>
+            <li className='item-price have'>
+              <p>Lorem ipsum isso tem</p>
+            </li>
+            
+            <li className='item-price not-have'>
+              <p>Lorem ipsum isso n tem</p>
+            </li>
+            <li className='item-price not-have'>
+              <p>Lorem ipsum isso n tem</p>
+            </li>
+            <li>
+              <button className='btn not'>Comprar</button>
+            </li>
+          </ul>
+        </div>
+        <div className='price-component terceiro'>
+        <div className='price-tag'>
+          <h1>Lorem Ipsum</h1>
+          <p className='price-price'><em className="pink-Lorem">R$ 149<small>/month</small></em></p>
+    </div>
+          <ul>
+            <li className='item-price have'>
+              <p>Lorem ipsum isso tem</p>
+            </li>
+            <li className='item-price have'>
+              <p>Lorem ipsum isso tem</p>
+            </li>
+            <li className='item-price have'>
+              <p>Lorem ipsum isso tem</p>
+            </li>
+            <li className='item-price have'>
+              <p>Lorem ipsum isso tem</p>
+            </li>
+            <li className='item-price have'>
+              <p>Lorem ipsum isso tem</p>
+            </li>
+            
+            <li>
+              <button className='btn not'>Comprar</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      </section>
+  </div>;
 };
 
 export default Price;
